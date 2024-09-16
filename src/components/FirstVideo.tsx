2351128268df035e3f02c73862d8e014505d2b22
Asmit tyagi <ashmittyagigms@gmail.com>
2024-09-16 08:32:03 +0530

    video section completely done

--- src/components/FirstVideo.tsx
+++ src/components/FirstVideo.tsx
@@ -2,22 +2,15 @@ const FirstVideo = () => {
 	return (
 		<div className="w-full flex flex-col justify-center items-center md:pt-12 py-5 rounded-full gap-8">
 			<div
-				className="relative flex justify-center md:w-[906px] md:h-[511px] w h-60 "
-				// style={{ width: "906px", height: "511px" }}
+				className="relative flex justify-center md:w-[906px] md:h-[511px] w-11/12 h-60 "
 			>
 				<iframe
 					id="titleVideo"
 					className="rounded-2xl w-full h-full"
-					// width="906"
-					// height="511"
 					src="https://www.youtube.com/embed/dQw4w9WgXcQ"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 					allowFullScreen
 				></iframe>
-				{/* <div
-					className="absolute top-0 left-0 w-full h-full"
-					style={{ cursor: "pointer", backgroundColor: "transparent" }}
-				></div> */}
 			</div>
 		</div>
 	);
